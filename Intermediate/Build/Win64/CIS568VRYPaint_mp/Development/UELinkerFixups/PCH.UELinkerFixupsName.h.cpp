374338c6fd5d74113c75f3b3a6593708f093a137
Carolina Zheng <swirlinq@gmail.com>
2017-02-24 00:35:52 -0500

    core stuff works but has bugs (aka features)

---
+++ Intermediate/Build/Win64/CIS568VRYPaint_mp/Development/UELinkerFixups/PCH.UELinkerFixupsName.h.cpp
@@ -0,0 +1,1 @@
+#include "C:\Users\SIG\Documents\Unreal Projects\CIS568VRYPaint_mp\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
---
+++ Intermediate/Build/Win64/CIS568VRYPaint_mp/Development/UELinkerFixups/UELinkerFixupsName.h
@@ -0,0 +1,5 @@
+#ifndef __AUTO_UELINKERFIXUPSNAME_H__
+#define __AUTO_UELINKERFIXUPSNAME_H__
+//Last Write: 2/22/2017 1:06:48 AM +00:00
+#include "C:\Users\SIG\Documents\Unreal Projects\CIS568VRYPaint_mp\Intermediate\Build\Win64\UE4\Development\UELinkerFixupsName.h"
+#endif//__AUTO_UELINKERFIXUPSNAME_H__
